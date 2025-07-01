31c084e9c47cc3f8dc0678e3c980aa2012f8c3d5
Usmon Khakim <usmonhakimov1999@gmail.com>
2025-07-01 09:41:46 +0500

    change halmet

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // Security middleware
-app.use(helmet());
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}));
 
 // CORS configuration
 app.use(cors({
